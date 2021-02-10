2e91ce5f86ba8417ac34a7d3b0bb3599b64e54eb
Chris <ChristianFigueroaMontano@outlook.com>
2021-02-09 21:34:48 -0600

    First commit

---
+++ routes/speciesRoute.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const Joi = require('joi');
+const router = express.Router();
+
+const data = require('../controllers/animals');
+
+
+//FUNCIONA 
+router.get('/', (req, res) => {
+    res.send(data);
+  });
+  //FUNCIONA
+  router.get('/:id', (req, res) => {
+    const {id} = req.params;
+    res.send(data.get(id));
+  });
+
+  module.exports = router;
